07c5688d10f107528ac2b9667865e554a32c9b94
NavyaSri3 <46093787+NavyaSri3@users.noreply.github.com>
2020-08-19 10:41:40 +0530

    Update SentimentScoreServlet.java

--- IdeaPortal/src/main/java/com/google/sps/servlets/SentimentScoreServlet.java
+++ IdeaPortal/src/main/java/com/google/sps/servlets/SentimentScoreServlet.java
@@ -27,7 +27,7 @@ public class SentimentScoreServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response, final long ProjectID) throws IOException {
 
-    List<Comment> comments= getListofCommentObject(final long ProjectID) //PreparedQuery results);
+    List<Comment> comments= getListofCommentObject(final long ProjectID) ;
 
     
     /**
@@ -35,7 +35,7 @@ public class SentimentScoreServlet extends HttpServlet {
     with the sentiment score.
     **/
       
-    }
+    
 
     Gson gson = new Gson();
 

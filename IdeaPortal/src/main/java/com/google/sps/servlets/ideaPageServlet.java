e3c3c4aa9e2e4f4fa871984a8f8aacc81042d730
Udbhav Chugh <chughudbhav@gmail.com>
2020-08-21 23:49:48 +0000

    Add idea showcase

--- IdeaPortal/src/main/java/com/google/sps/servlets/ideaPageServlet.java
+++ IdeaPortal/src/main/java/com/google/sps/servlets/ideaPageServlet.java
@@ -9,6 +9,7 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query.CompositeFilter;
 import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
@@ -31,34 +32,35 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ideaPage")
 public class ideaPageServlet extends HttpServlet { 
 
-  private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private static DatastoreService datastore;
+  @Override
+  public void init(){
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    long ProductID= Long.parseLong(request.getParameter("ProductID"));
+    long ProductID= Long.parseLong(request.getParameter("productid"));
 
-    Filter ProductIDFilter = new FilterPredicate("ProductID", FilterOperator.EQUAL, ProductID);
+    Filter ProductIDFilter = new FilterPredicate("__key__", FilterOperator.EQUAL, KeyFactory.createKey("ProductIdea",ProductID));
     Query query = new Query("ProductIdea").setFilter(ProductIDFilter);
-    PreparedQuery results = datastore.prepare(query);
+    List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+    Entity entity = results.get(0);
 
-    List<ProductIdea> productIdeas = new ArrayList<>();
-    for (Entity entity : results.asIterable()) {
-      long authorId = (long) entity.getProperty("authorId");
-      String title = (String) entity.getProperty("title");
-      long timestamp = (long) entity.getProperty("timestamp");
-      String category = (String) entity.getProperty("category");
-      String imageUrl = (String) entity.getProperty("imageUrl");
-      String description = (String) entity.getProperty("description");
-      
-      ProductIdea productidea = new ProductIdea(ProductID, title, authorId, timestamp, category, imageUrl, description);
-      productIdeas.add(productidea);
-    }
+    long productID = (long) entity.getKey().getId();
+    String title = (String) entity.getProperty("title");
+    long authorID = (long) entity.getProperty("authorid");
+    long timestamp = (long) entity.getProperty("timestamp");
+    String category = (String) entity.getProperty("category");
+    String imageUrl = (String) entity.getProperty("imageUrl");
+    String description = (String) entity.getProperty("description");
+    
+    ProductIdea finalProductIdea = new ProductIdea(productID, title, authorID, timestamp, category, imageUrl, description);
     
     Gson gson = new Gson();
-
     response.setContentType("application/json");
-    response.getWriter().println(gson.toJson(productIdeas));
+    response.getWriter().println(gson.toJson(finalProductIdea));
   } 
 
 }
--- IdeaPortal/src/main/webapp/scripts/idea.js
+++ IdeaPortal/src/main/webapp/scripts/idea.js
@@ -1,56 +1,82 @@
+function getcardTitle(idea){
+    var cardTitle = document.createElement("h4");
+    cardTitle.className = "card-title";
+    var titleText = document.createTextNode(idea.title);
+    cardTitle.appendChild(titleText);
+    return cardTitle;
+}
+
+function getCardDescription(idea){
+    var cardDescription = document.createElement("b");
+    cardDescription.className = "card-text";
+    var descriptionText = document.createTextNode(idea.description);
+    cardDescription.appendChild(descriptionText);
+    return cardDescription;
+}
+
+function getStatsLink(idea){
+    var statsLink = document.createElement("button");
+    statsLink.className = "btn btn-dark";
+    var link = document.createTextNode("Analyse Statistics");
+    statsLink.appendChild(link);
+    // statsLink.href = "/charts.html";
+    statsLink.onclick = function () {
+        localStorage.setItem("productid",idea.productID);
+        location.href = "/charts.html";
+    };
+    return statsLink;
+}
+
+function getCardBody(idea){
+    var cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    
+    var cardTitle = getcardTitle(idea);
+    var cardDescription = getCardDescription(idea);
+    var lineBreak = document.createElement("br");
+    var statsLink = getStatsLink(idea);
+    var lineBreak2 = document.createElement("br");
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardDescription);
+    cardBody.appendChild(lineBreak);
+    cardBody.appendChild(statsLink);
+    cardBody.appendChild(lineBreak2);
+    return cardBody;
+}
+
+function getIdeaCard(idea){
+    var ideaCard = document.createElement("div");
+    ideaCard.className = "card mb-3 text-center";
+    ideaCard.style.width = "22rem";
+    ideaCard.style.margin = "1.5em";
+    if(idea.imageUrl){
+        var img = document.createElement("img"); 
+        img.className = "card-img-top";
+        img.src =  idea.imageUrl;
+        img.width = "400"
+        img.height = "300"
+        ideaCard.appendChild(img);
+    }
+    ideaCard.appendChild(getCardBody(idea));
+    return ideaCard;
+}
+
+function getIdeaDiv(idea){
+    var ideaDiv = document.createElement("div");
+    ideaDiv.style.display = "inline-block";
+    ideaDiv.appendChild(getIdeaCard(idea));    
+    return ideaDiv;
+}
+
 
 function fetchIdea() {
-
-    fetch('/ideaPage').then(response => response.json()).then((idea) => {
-
-    console.log(idea);
-
-    const ideaTitleElement = document.getElementById('idea-title');
-    ideaTitleElement.innerHTML = '';
-    ideaTitleElement.appendChild(createHeadingh1Element(idea.title));  
-
-    const ideaAuthorElement = document.getElementById('author');
-    ideaAuthorElement.innerHTML = '';
-    ideaAuthorElement.appendChild(createHeadingh2Element(idea.authorID));  
-
-    const ideaDescriptionElement = document.getElementById('idea-description');
-    ideaTitleElement.innerHTML = '';
-    ideaDescriptionElement.appendChild(createParaElement(idea.description));  
-
-
-    const ideaImageElement = document.getElementById('image');
-    var img  = document.createElement('img');
-    img.src = idea.imageURL;
-    ideaImageElement.appendChild(img);
-
-    const ideaCategoriesElement = document.getElementById('idea-categories');
-    ideaCategoriesElement.innerHTML = '';
-    ideaCategoriesElement.appendChild(createListElement(idea.category));  		
-
-    
+  productid = localStorage.getItem("productid");
+  fetch('/ideaPage?productid=' + productid).then(response => response.json()).then((idea) => {
+      console.log("he");
+    var ideasContainer = document.getElementById('ideainfo-container');
+    // ideasContainer.appendChild(ideaDiv);
+    var ideaDiv = getIdeaDiv(idea);
+    ideasContainer.appendChild(ideaDiv);
   });
-}
-
-function createParaElement(text) {
-  const ParaElement = document.createElement('p');
-  ParaElement.innerText = text;
-  return ParaElement;
-}
-
-function createHeadingh1Element(text) {
-  const Headingh1Element = document.createElement('H1');
-  Headingh1Element.innerText = text;
-  return Headingh1Element;
-}
-
-function createHeadingh2Element(text) {
-  const Headingh2Element = document.createElement('H2');
-  Headingh2Element.innerText = text;
-  return Headingh2Element;
-}
-
-function createListElement(text) {
-  const ParaElement = document.createElement('li');
-  ParaElement.innerText = text;
-  return ListElement;
 }

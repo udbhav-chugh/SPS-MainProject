e208e7a5e968692070641bad25a1ea0bb12e7db8
NavyaSri3 <46093787+NavyaSri3@users.noreply.github.com>
2020-08-22 04:43:34 +0530

    projectID updated to productID

--- IdeaPortal/src/main/java/com/google/sps/data/Survey.java
+++ IdeaPortal/src/main/java/com/google/sps/data/Survey.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 import java.util.ArrayList; 
 
 public final class Survey{
-    private final long ProjectID;
+    private final long productID;
     private final long authorId;
     private int ageGroupCount; //0-14, 15-24, 25-65, >65
 
-    public Survey(long ProjectID, long authorId){
-        this.ProjectID= ProjectID;
+    public Survey(long productID, long authorId){
+        this.productID= productID;
         this.authorId = authorId;
         this.ageGroupCount = -1;
         

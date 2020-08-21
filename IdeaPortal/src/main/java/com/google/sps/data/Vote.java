b7581e4a7e94334d624e9a9ea19b4149d601d67c
NavyaSri3 <46093787+NavyaSri3@users.noreply.github.com>
2020-08-22 04:44:27 +0530

    projectID updated to productID

--- IdeaPortal/src/main/java/com/google/sps/data/Vote.java
+++ IdeaPortal/src/main/java/com/google/sps/data/Vote.java
@@ -1,12 +1,12 @@
 package com.google.sps.data;
 
 public final class Vote{
-    private final long ProjectID;
+    private final long productID;
     private final long userId;
     private int voteValue; // -1-> downvote , 0 -> no vote, 1 -> upvote
 
-    public Vote(long ProjectID,long userId, int voteValue){
-        this.ProjectID= ProjectID;
+    public Vote(long productID,long userId, int voteValue){
+        this.productID= productID;
         this.userId = userId;
         this.voteValue = voteValue;
     }

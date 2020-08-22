cb24bfdd7c2079c4b247f713ce76cd7246558961
Udbhav Chugh <chughudbhav@gmail.com>
2020-08-22 02:08:56 +0000

    Update Idea Page UI

--- IdeaPortal/src/main/java/com/google/sps/data/Survey.java
+++ IdeaPortal/src/main/java/com/google/sps/data/Survey.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 public final class Survey{
     private final long ProjectID;
     private final long authorId;
-    private ArrayList<Integer> ageGroupCount; //0-14, 15-24, 25-65, >65
+    Integer ageGroupCount; //0-14, 15-24, 25-65, >65 //Represents index of age group from 0 to 3
 
-    public Survey(long ProjectID, long authorId){
+    public Survey(long ProjectID, long authorId, Integer ageGroupCount){
         this.ProjectID= ProjectID;
         this.authorId = authorId;
-        ageGroupCount = new ArrayList<Integer>();
-        for(int i=0;i<4;i++)
-            ageGroupCount.add(0);
+        this.ageGroupCount = ageGroupCount;
     }
 
     //Will add getters and setters as per requirement

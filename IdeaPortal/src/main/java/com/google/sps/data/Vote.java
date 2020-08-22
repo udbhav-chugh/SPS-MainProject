8772e81a75f35cba19608555c19bb931ee69e92f
Udbhav Chugh <chughudbhav@gmail.com>
2020-08-22 03:35:31 +0000

    change userid to authorId

--- IdeaPortal/src/main/java/com/google/sps/data/Vote.java
+++ IdeaPortal/src/main/java/com/google/sps/data/Vote.java
@@ -2,12 +2,12 @@ package com.google.sps.data;
 
 public final class Vote{
     private final long productID;
-    private final long userId;
+    private final long authorId;
     private Integer voteValue; // -1-> downvote , 0 -> no vote, 1 -> upvote
 
-    public Vote(long productID,long userId, Integer voteValue){
+    public Vote(long productID,long authorId, Integer voteValue){
         this.productID= productID;
-        this.userId = userId;
+        this.authorId = authorId;
         this.voteValue = voteValue;
     }
 
